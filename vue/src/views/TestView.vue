d86db202e58a149336df51a35e0ddd5788f291ed
chaeriz <chrisobt@outlook.com>
2023-05-12 10:11:32 +0200

    init

---
+++ vue/src/views/TestView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        This is a test page with a form
+        <form action="">
+        <input type="text" name="" id="" placeholder="username">
+        <input type="text" name="" id="" placeholder="password">
+        <button>Submit</button>
+    </form>
+    </div>
+    
+</template>
+
